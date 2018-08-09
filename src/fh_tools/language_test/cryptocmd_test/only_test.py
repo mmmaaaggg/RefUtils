479de5c4016d2f6e3a876aa643365569b595836f
mmmaaaggg <mmmaaaggg@163.com>
2018-08-09 18:25:06 +0800

    cmc test

--- src/fh_tools/language_test/cryptocmd_test/only_test.py
+++ src/fh_tools/language_test/cryptocmd_test/only_test.py
@@ -11,13 +11,14 @@
 from cryptocmd import CmcScraper
 
 # initialise scraper without passing time interval
-scraper = CmcScraper('XRP')
+coin_code = 'EOS'
+scraper = CmcScraper(coin_code)
 
 # data as list of list in a variable
 headers, data = scraper.get_data()
 
 # export the data as csv file, you can also pass optional name parameter
-scraper.export_csv('xrp_all_time.csv')
+scraper.export_csv('{coin_code}_all_time.csv'.format(coin_code=coin_code))
 
 # Pandas dataFrame for the same data
 df = scraper.get_dataframe()
