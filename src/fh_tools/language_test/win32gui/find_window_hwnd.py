ea5f8e431cde2cfdcf5eedc604bab2d1024f2c2f
mmmaaaggg <mmmaaaggg@163.com>
2018-09-04 17:04:19 +0800

    增加window 工具函数

--- src/fh_tools/language_test/win32gui/find_window_hwnd.py
+++ src/fh_tools/language_test/win32gui/find_window_hwnd.py
@@ -5,6 +5,7 @@ Created on 2017/9/19
 """
 import win32gui
 import re
+from functools import partial
 
 
 # def _filter_trade_client(hwnd, hwnd_list, filter_func):
@@ -14,7 +15,7 @@ import re
 
 def filter_func(hwnd):
     # 找到classname = '#32770' 的窗体
-    re_classname_pattern = '#32770'
+    re_classname_pattern = 'ToolbarWindow32'
     clsname = win32gui.GetClassName(hwnd)
     if re.match(re_classname_pattern, clsname) is None:
         return False
@@ -32,16 +33,45 @@ def filter_func(hwnd):
 
 
 def find_window_whnd(filter_func, ret_first=True):
+    # 输入 filter_func 查找符合条件的全部或首个窗口whnd
     window_hwnd = None
     hwnd_list = []
     win32gui.EnumWindows(lambda hwnd, hwnd_list_tmp: hwnd_list_tmp.append(hwnd) if filter_func(hwnd) else hwnd,
                          hwnd_list)
-    if ret_first:
-        if len(hwnd_list) > 0:
+    if len(hwnd_list) > 0:
+        if ret_first:
             window_hwnd = hwnd_list[0]
-        return window_hwnd
+            return window_hwnd
+        else:
+            return hwnd_list
     else:
-        return hwnd_list
+        return None
+
+
+def filter_hwnd_func(hwnd, contain_window_text):
+    """ 查找 内部包含“contain_window_text”标签的框体"""
+    try:
+        # 找到classname = '#32770' 的窗体
+        re_classname_pattern = '#32770'
+        clsname = win32gui.GetClassName(hwnd)
+        if re.match(re_classname_pattern, clsname) is None:
+            return False
+        # 查找 内部包含“可用金额”标签的框体
+        hwnd_chld_list = []
+        win32gui.EnumChildWindows(hwnd, lambda hwnd_sub, hwnd_chld_list_tmp: hwnd_chld_list_tmp.append(hwnd_sub),
+                                  hwnd_chld_list)
+        for hwnd_sub in hwnd_chld_list:
+            if win32gui.GetClassName(hwnd_sub) == 'Static' and win32gui.GetWindowText(hwnd_sub) == contain_window_text:
+                return True
+    except:
+        pass
+    return False
+
+
+def filter_by_win_title(hwnd, contain_window_text):
+    """ 查找 内部包含“contain_window_text”标签的框体"""
+    clsname = win32gui.GetClassName(hwnd)
+    return contain_window_text == clsname
 
 
 if __name__ == "__main__":
--- src/fh_tools/win32_utils.py
+++ src/fh_tools/win32_utils.py
@@ -3,8 +3,13 @@
 Created on 2017/4/12
 @author: MG
 """
+import win32api
+import win32con
 import win32gui
 import pywintypes
+import pyautogui
+import time
+from ctypes import windll
 
 
 def find_hwnds_by_lambdas(lambda_list, hwnd_parent=None):
@@ -25,15 +30,15 @@ def find_hwnds_by_lambdas(lambda_list, hwnd_parent=None):
         hwnd = win32gui.FindWindowEx(hwnd_parent, hwnd, None, None)
         if hwnd == 0:
             break
-            is_ok = func(hwnd)
-            if is_ok:
-                hwnd_list_ret.append(hwnd)
-                if lambda_count > 1:
-                    hwnd_list = find_hwnds_by_lambdas(lambda_list[1:], hwnd_parent=hwnd)
-                    if hwnd_list is None or len(hwnd_list) == 0:
-                        continue
-                    hwnd_list_ret.extend(hwnd_list)
-                return hwnd_list_ret
+        is_ok = func(hwnd)
+        if is_ok:
+            hwnd_list_ret.append(hwnd)
+            if lambda_count > 1:
+                hwnd_list = find_hwnds_by_lambdas(lambda_list[1:], hwnd_parent=hwnd)
+                if hwnd_list is None or len(hwnd_list) == 0:
+                    continue
+                hwnd_list_ret.extend(hwnd_list)
+            return hwnd_list_ret
     return None
 
 
@@ -87,19 +92,49 @@ def find_hwnds_by_matchers(matcher_list, hwnd_parent=None):
     return None
 
 
+def mouse_move(x, y):
+    windll.user32.SetCursorPos(x, y)
+
+
+def mouse_click(x=None, y=None):
+    if not x is None and not y is None:
+        mouse_move(x, y)
+        time.sleep(0.05)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+
 if __name__ == '__main__':
     # 以恒泰证券交易端界面为测试用例
-    matcher_list = [('window', lambda x: win32gui.GetClassName(x).find('Afx:400000') == 0),
-                    ('window', lambda x: win32gui.GetClassName(x).find('Afx:400000') == 0),
-                    ('DlgItem', 0xE900),
-                    ('DlgItem', 0xE900),
-                    ('DlgItem', 0x81),
-                    ('DlgItem', 0xC8),
-                    ('DlgItem', 0x81),
+    # matcher_list = [('window', lambda x: win32gui.GetClassName(x).find('Afx:400000') == 0),
+    #                 ('window', lambda x: win32gui.GetClassName(x).find('Afx:400000') == 0),
+    #                 ('DlgItem', 0xE900),
+    #                 ('DlgItem', 0xE900),
+    #                 ('DlgItem', 0x81),
+    #                 ('DlgItem', 0xC8),
+    #                 ('DlgItem', 0x81),
+    #                 ]
+    # 找到任务栏图标
+    matcher_list = [('window', lambda x: win32gui.GetClassName(x).find('Shell_TrayWnd') == 0),
+                    ('window', lambda x: win32gui.GetClassName(x).find('TrayNotifyWnd') == 0),
+                    ('window', lambda x: win32gui.GetClassName(x).find('SysPager') == 0),
+                    ('window', lambda x: win32gui.GetClassName(x).find('ToolbarWindow32') == 0 and
+                                         win32gui.GetWindowText(x) == '用户提示通知区域'),
                     ]
-    # lambda_list = [lambda x: win32gui.GetClassName(x).find('Afx:400000') == 0,
-    #                lambda x: win32gui.GetClassName(x).find('Afx:400000') == 0,
-    #                ]
     # hwnd_list = find_hwnds_by_lambdas(lambda_list)
     hwnd_list = find_hwnds_by_matchers(matcher_list)
-    print(hwnd_list)
+    # 该列表有四个句柄，取 win32gui.GetWindowRect(0x100ea)  第一个坐标最靠右的一个
+    hwnd_rect_dic = {hwnd: win32gui.GetWindowRect(hwnd) for hwnd in hwnd_list}
+    max_rect_left = max([rect[0] for rect in hwnd_rect_dic.values()])
+    for hwnd_target, (rect_left, rect_top, _, _) in hwnd_rect_dic.items():
+        if max_rect_left == rect_left:
+            break
+    print(['%#x' % hwnd for hwnd in hwnd_list])
+    print('goto %d %d' % (rect_left, rect_top))
+    # pyautogui.moveTo(rect_left + 10, rect_top + 10)
+    pyautogui.click(rect_left + 10, rect_top + 10, button='right')
+    time.sleep(1)
+    # pyautogui.moveTo(rect_left + 30, rect_top - 10)
+    # pyautogui.click(rect_left + 30, rect_top - 10)
+    mouse_click(rect_left - 30, rect_top - 10)
+
