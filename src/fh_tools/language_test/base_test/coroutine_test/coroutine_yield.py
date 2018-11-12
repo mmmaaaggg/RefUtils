bc824fcd6d40061e97c405598d8c0c1e5010a4b0
mmmaaaggg <mmmaaaggg@163.com>
2018-11-12 14:35:01 +0800

    add example

---
+++ src/fh_tools/language_test/base_test/coroutine_test/coroutine_yield.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/11/12 9:21
+@File    : coroutine_yield.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
+
+def cor():
+    data_list = []
+    while True:
+        print('get data')
+        x = yield
+        print('get data', x)
+        if x is None:
+            break
+        data_list.append(x)
+    ret = sum(data_list) / len(data_list)
+    print('average', ret)
+    return ret
+
+
+def main():
+    print('call cor()')
+    func = cor()
+    print('next(func)')
+    next(func)
+    try:
+        for x in range(4):
+            func.send(x)
+
+        func.send(None)
+    except StopIteration as exp:
+        print('func return', exp.value)
+
+
+if __name__ == "__main__":
+    main()
