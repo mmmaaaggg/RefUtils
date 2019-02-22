1ac26d63d29533cdb371771af5a7cea372c1397c
mmmaaaggg <mmmaaaggg@163.com>
2019-02-22 17:35:57 +0800

    add tensorflow demo

---
+++ src/fh_tools/language_test/TensorFlow_test/base_test/rnn_regression_demo.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2019/02/22 下午5:34
+@File    : rnn_regression_demo.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
