a0f2f6b4c84f5c0f8411ec97ca2e28703f5ef76a
mmmaaaggg <mmmaaaggg@163.com>
2019-02-22 18:50:27 +0800

    add tensorflow demo

--- src/fh_tools/language_test/tensorflow_demo/base_demo/rnn_regression_demo.py
+++ src/fh_tools/language_test/tensorflow_demo/base_demo/rnn_regression_demo.py
@@ -7,4 +7,43 @@
 @contact : mmmaaaggg@163.com
 @desc    : 
 """
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+BATCH_START = 0
+TIME_STEPS = 20
+BATCH_SIZE = 50
+INPUT_SIZE = 1
+OUTPUT_SIZE = 1
+CELL_SIZE = 10
+LR = 0.006
+BATCH_START_TEST = 0
+
+
+def get_batch():
+    global BATCH_START, TIME_STEPS
+    # xs shape(50 batch, 20 steps)
+    xs = np.arange(BATCH_START, BATCH_START + TIME_STEPS * BATCH_SIZE).reshape((BATCH_SIZE, TIME_STEPS))
+    seq = np.sin(xs)
+    res = np.cos(xs)
+    BATCH_START += TIME_STEPS
+    # plt.plot(xs[0, :], res[0, :], 'r', xs[0, :], seq[0, :], 'b--')
+    # plt.show()
+    # returned seq, res and shape (batch, step, input)
+    return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
+
+
+class LSTMRNN:
+    def __init__(self, n_step, input_size, output_size, cell_size, batch_size):
+        self.n_step = n_step
+        self.input_size = input_size
+        self.output_size = output_size
+        self.cell_size = cell_size
+        self.batch_size = batch_size
+        with tf.name_scope('inputs'):
+            self.xs = tf.placeholder(tf.float32, [None, n_step, input_size])
+
+
+if __name__ == "__main__":
+    get_batch()
