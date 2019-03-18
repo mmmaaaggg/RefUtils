c32e02852eb53649c438fb7bca3bf413be3a491f
mmmaaaggg <mmmaaaggg@163.com>
2019-03-18 18:12:13 +0800

    add quant utils

--- src/fh_tools/quant.py
+++ src/fh_tools/quant.py
@@ -8,12 +8,13 @@
 @desc    : 量化分析相关工具
 """
 import pandas as pd
+import numpy as np
 
 
-def target_futrue_pct_range(df: pd.DataFrame, pct_label: str, min_pct: float, max_pct: float, target_label='target'):
+def add_target_future_pct_range(df: pd.DataFrame, pct_label: str, min_pct: float, max_pct: float, target_label='target'):
     """
     根据时间序列数据 df[pct_label] 计算每一个时点目标标示 -1 0 1
-    计算方式：当某一点未来波动首先
+    计算方式：当某一点未来波动首先 >（ 或 <） 上届 min_pct（或下届 max_pct），则标记为 1 （或 -1）
     :param df:
     :param pct_label:
     :param min_pct:
@@ -39,10 +40,41 @@ def target_futrue_pct_range(df: pd.DataFrame, pct_label: str, min_pct: float, ma
     return df
 
 
+def get_target_by_future_pct_range(pct_arr: np.ndarray, min_pct: float, max_pct: float):
+    """
+    根据时间序列数据 pct_arr 计算每一个时点目标标示 -1 0 1
+    计算方式：当某一点未来波动首先 >（ 或 <） 上届 min_pct（或下届 max_pct），则标记为 1 （或 -1）
+    :param pct_arr:
+    :param min_pct:
+    :param max_pct:
+    :return:
+    """
+    pct_arr[np.isnan(pct_arr)] = 0
+    cum_rr = (pct_arr + 1).cumprod()
+    arr_len = cum_rr.shape[0]
+    target_arr = np.zeros(cum_rr.shape)
+    for i in range(arr_len):
+        base = cum_rr[i]
+        for j in range(i + 1, arr_len):
+            result = cum_rr[j] / base - 1
+            if result < min_pct:
+                target_arr[i] = -1
+                break
+            elif result > max_pct:
+                target_arr[i] = 1
+                break
+
+    return target_arr
+
+
 if __name__ == "__main__":
-    import numpy as np
 
     df = pd.DataFrame({"price": np.sin(np.arange(0, 7, 0.3)) + 5})
     df['pct_change'] = df['price'].pct_change()
-    df_target = target_futrue_pct_range(df, 'pct_change', -0.1, 0.1)
-    print(df_target)
+    df_target = add_target_future_pct_range(df, 'pct_change', -0.1, 0.1)
+    print("df_target:\n", df_target)
+
+    pct_arr = df['pct_change'].to_numpy()
+    target_arr = get_target_by_future_pct_range(pct_arr, -0.1, 0.1)
+    print("target_arr:\n", target_arr)
+
