dccb4b589e2b5ee4e156c3de81a73516a329397f
mmmaaaggg <mmmaaaggg@163.com>
2019-03-20 10:54:14 +0800

    add example

--- src/fh_tools/language_test/itertools_test/produce_test.py
+++ src/fh_tools/language_test/itertools_test/produce_test.py
@@ -2,5 +2,12 @@ from itertools import product
 
 iter_func = range(3)
 iter_result = product(iter_func, repeat=3)
+print("product(iter_func, repeat=3)")
 for comp in iter_result:
     print(comp)
+
+iter_result = product(range(2), range(3), range(4))
+print("product(range(2), range(3), range(4))")
+for comp in iter_result:
+    print(comp)
+
