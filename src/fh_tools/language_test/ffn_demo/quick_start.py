6328f10c7b1794246c9bc8ea2275911cd7950841
mmmaaaggg <mmmaaaggg@163.com>
2019-05-10 18:36:40 +0800

    add ffn demo

--- src/fh_tools/language_test/ffn_demo/quick_start.py
+++ src/fh_tools/language_test/ffn_demo/quick_start.py
@@ -8,7 +8,28 @@
 @desc    : http://pmorissette.github.io/ffn/quick.html
 """
 import ffn
+import matplotlib.pyplot as plt
 
-data = ffn.get('agg,hyg,spy,eem,efa', start='2010-01-01', end='2014-01-01')
-print(data.head())
+df = ffn.get('agg,hyg,spy,eem,efa', start='2010-01-01', end='2014-01-01')
+print(df.head())
 
+# 归一化走势图
+ax = df.rebase().plot()
+plt.show()
+
+# 收益率分布图
+returns = df.to_returns().dropna()
+ax = returns.hist()
+plt.show()
+
+stats = df.calc_stats()
+print("data.calc_stats().display()\n")
+stats.display()
+
+# 回撤图
+ax = stats.prices.to_drawdown_series().plot()
+plt.show()
+
+stats = df['hyg'].calc_perf_stats()
+print("data['hyg'].calc_perf_stats().display()")
+stats.display()
