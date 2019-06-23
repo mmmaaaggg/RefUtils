bff355bd7d7059d10526404be8774d0c65fb2aab
mmmaaaggg <mmmaaaggg@163.com>
2019-06-23 18:19:45 +0800

    add AC A2C examples

--- src/fh_tools/language_test/reinforcement_learning_demo/AC_A2C_A3C_demo/A2C.py
+++ src/fh_tools/language_test/reinforcement_learning_demo/AC_A2C_A3C_demo/A2C.py
@@ -2,10 +2,10 @@
 # -*- coding:utf-8 -*-
 """
 @author  : MG
-@Time    : 2019/6/23 8:23
-@File    : AC_demo.py
+@Time    : 2019/6/23 11:19
+@File    : A2C.py.py
 @contact : mmmaaaggg@163.com
-@desc    : https://github.com/princewen/tensorflow_practice/blob/master/RL/Basic-AC-Demo/AC.py
+@desc    : 
 """
 import numpy as np
 import tensorflow as tf
@@ -16,10 +16,10 @@ OUTPUT_GRAPH = False
 MAX_EPISODE = 500
 DISPLAY_REWARD_THRESHOLD = 200  # renders environment if total episode reward is greater then this threshold
 MAX_EP_STEPS = 2000  # maximum time step in one episode
-RENDER = True  # rendering wastes time
+RENDER = False  # rendering wastes time
 GAMMA = 0.9  # reward discount in TD error
 LR_A = 0.001  # learning rate for actor
-LR_C = 0.001  # learning rate for critic
+LR_C = 0.01  # learning rate for critic
 
 
 class Actor(object):
@@ -28,7 +28,7 @@ class Actor(object):
 
         self.s = tf.placeholder(tf.float32, [1, n_features], "state")
         self.a = tf.placeholder(tf.int32, None, "action")
-        self.q = tf.placeholder(tf.float32, None, "q")  # TD_error
+        self.td_error = tf.placeholder(tf.float32, None, "td_error")  # TD_error
 
         with tf.variable_scope('Actor'):
             l1 = tf.layers.dense(
@@ -51,43 +51,34 @@ class Actor(object):
 
         with tf.variable_scope('exp_v'):
             log_prob = tf.log(self.acts_prob[0, self.a])
-            self.exp_v = tf.reduce_mean(log_prob * self.q)  # advantage (TD_error) guided loss
+            self.exp_v = tf.reduce_mean(log_prob * self.td_error)  # advantage (TD_error) guided loss
 
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v)  # minimize(-exp_v) = maximize(exp_v)
 
-    def learn(self, s, a, q):
+    def learn(self, s, a, td):
         s = s[np.newaxis, :]
-        feed_dict = {self.s: s, self.a: a, self.q: q}
+        feed_dict = {self.s: s, self.a: a, self.td_error: td}
         _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict)
         return exp_v
 
     def choose_action(self, s):
         s = s[np.newaxis, :]
         probs = self.sess.run(self.acts_prob, {self.s: s})  # get probabilities for all actions
-        p_value = probs.ravel()
-        choice = np.random.choice(np.arange(probs.shape[1]), p=p_value)  # return a int
-        print("probs.ravel():", probs.ravel(), "choice:", choice)
-        return choice
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())  # return a int
 
 
 class Critic(object):
-    def __init__(self, sess, n_features, n_actions, lr=0.01):
+    def __init__(self, sess, n_features, lr=0.01):
         self.sess = sess
 
-        self.s = tf.placeholder(tf.float32, [None, n_features], "state")
-        self.a = tf.placeholder(tf.int32, [None, 1], "action")
+        self.s = tf.placeholder(tf.float32, [1, n_features], "state")
+        self.v_ = tf.placeholder(tf.float32, [1, 1], "v_next")
         self.r = tf.placeholder(tf.float32, None, 'r')
-        self.q_ = tf.placeholder(tf.float32, [None, 1], 'q_next')
-
-        self.a_onehot = tf.one_hot(self.a, n_actions, dtype=tf.float32)
-        self.a_onehot = tf.squeeze(self.a_onehot, axis=1)
-
-        self.input = tf.concat([self.s, self.a_onehot], axis=1)
 
         with tf.variable_scope('Critic'):
             l1 = tf.layers.dense(
-                inputs=self.input,
+                inputs=self.s,
                 units=20,  # number of hidden units
                 activation=tf.nn.relu,  # None
                 kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
@@ -95,30 +86,28 @@ class Critic(object):
                 name='l1'
             )
 
-            self.q = tf.layers.dense(
+            self.v = tf.layers.dense(
                 inputs=l1,
                 units=1,  # output units
                 activation=None,
                 kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
                 bias_initializer=tf.constant_initializer(0.1),  # biases
-                name='Q'
+                name='V'
             )
 
         with tf.variable_scope('squared_TD_error'):
-            self.td_error = self.r + GAMMA * self.q_ - self.q
+            self.td_error = self.r + GAMMA * self.v_ - self.v
             self.loss = tf.square(self.td_error)  # TD_error = (r+gamma*V_next) - V_eval
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
 
-    def learn(self, s, a, r, s_):
+    def learn(self, s, r, s_):
         s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
-        next_a = [[i] for i in range(N_A)]
-        s_ = np.tile(s_, [N_A, 1])
-        q_ = self.sess.run(self.q, {self.s: s_, self.a: next_a})
-        q_ = np.max(q_, axis=0, keepdims=True)
-        q, _ = self.sess.run([self.q, self.train_op],
-                             {self.s: s, self.q_: q_, self.r: r, self.a: [[a]]})
-        return q
+
+        v_ = self.sess.run(self.v, {self.s: s_})
+        td_error, _ = self.sess.run([self.td_error, self.train_op],
+                                    {self.s: s, self.v_: v_, self.r: r})
+        return td_error
 
 
 # action有两个，即向左或向右移动小车
@@ -134,7 +123,7 @@ N_A = env.action_space.n
 sess = tf.Session()
 
 actor = Actor(sess, n_features=N_F, n_actions=N_A, lr=LR_A)
-critic = Critic(sess, n_features=N_F, n_actions=N_A, lr=LR_C)
+critic = Critic(sess, n_features=N_F, lr=LR_C)
 
 sess.run(tf.global_variables_initializer())
 
@@ -154,8 +143,8 @@ for i_episode in range(MAX_EPISODE):
 
         track_r.append(r)
 
-        q = critic.learn(s, a, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
-        actor.learn(s, a, q)  # true_gradient = grad[logPi(s,a) * td_error]
+        td_error = critic.learn(s, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
+        actor.learn(s, a, td_error)  # true_gradient = grad[logPi(s,a) * td_error]
 
         s = s_
         t += 1
@@ -170,7 +159,6 @@ for i_episode in range(MAX_EPISODE):
             if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True  # rendering
             print("episode:", i_episode, "  reward:", int(running_reward))
             res.append([i_episode, running_reward])
-
             break
 
-pd.DataFrame(res, columns=['episode', 'ac_reward']).to_csv('../ac_reward.csv')
+pd.DataFrame(res, columns=['episode', 'a2c_reward']).to_csv('../a2c_reward.csv')
