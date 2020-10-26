6aaba5066e179d5628652f8ffb9e697614d0bb20
mmmaaaggg <mmmaaaggg@163.com>
2020-10-26 10:39:37 +0800

    feat: global nonlocal key word

--- src/fh_tools/language_test/base_test/nonlocal_test.py
+++ src/fh_tools/language_test/base_test/nonlocal_test.py
@@ -2,7 +2,12 @@
 """
 Created on 2017/9/30
 @author: MG
+全局作用域（global）：在整个程序运行环境中都可见
+@description: nonlocal global 两个关键字
+nonlocal，指定上一级变量，如果没有就继续往上直到找到为止
+global, 函数中有global关键字，变量本质上就是全局变量，可读取可赋值
 """
+
 
 def make_averager():
     count = 0
@@ -15,6 +20,7 @@ def make_averager():
         return total / count
 
     return averager
+
 
 if __name__ == "__main__":
     avg = make_averager()
